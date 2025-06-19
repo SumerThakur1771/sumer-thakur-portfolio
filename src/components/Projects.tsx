9064d7f2a0671f1732110e1553910b6d3e6f676a
Sumer Thakur <thakur.sum@northeastern.edu>
2025-06-19 17:20:32 -0400

    edited object contain to coveri in project image

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -48,7 +48,7 @@ const Projects = () => {
                     <img 
                       src={project.image} 
                       alt={project.title}
-                      className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
+                      className="w-full h-full object-contain group-hover:scale-110 transition-transform duration-500"
                     />
                   </div>
                   <div className="absolute inset-0 bg-gradient-to-t from-purple-500/20 to-transparent rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
